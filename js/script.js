fb78f6040e30b51ae1d0c50fe3c193ff7cd243da
Mateusz Barej <mateusz.barej89@gmail.com>
2023-06-22 13:45:44 +0200

    Add code from codepen

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber === 1) {
-  computerMove = 'kamień';
-} else if (randomNumber === 2) {
-  computerMove = 'papier';
-} else if (randomNumber === 3) {
-  computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
-}
-console.log('Mój ruch: ' + computerMove);
-
-var playerMove, playerInput;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput === '1') {
-  playerMove = 'kamień';
-} else if (playerInput === '2') {
-  playerMove = 'papier';
-} else if (playerInput === '3') {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'kamień';
-  printMessage('Błędny wybór! Twój ruch został ustawiony na kamień.');
-}
-console.log('Twój ruch: ' + playerMove);
+// Funkcja generująca losowy wybór komputera
+function computerChoice() {
+    const choices = ["kamień", "papier", "nożyce"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  // Funkcja porównująca wybory gracza i komputera oraz zwracająca wynik rundy
+  function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return "Remis! Oboje wybraliście " + playerChoice + ".";
+    } else if (
+      (playerChoice === "kamień" && computerChoice === "nożyce") ||
+      (playerChoice === "papier" && computerChoice === "kamień") ||
+      (playerChoice === "nożyce" && computerChoice === "papier")
+    ) {
+      return "Wygrałeś rundę! Twój " + playerChoice + " pokonuje " + computerChoice + ".";
+    } else {
+      return "Przegrałeś rundę! Twój " + playerChoice + " jest pokonany przez " + computerChoice + ".";
+    }
+  }
+  
+  // Funkcja rozpoczynająca grę
+  function startGame() {
+    const playerChoice = prompt("Wybierz kamień, papier lub nożyce:").toLowerCase();
+    
+    if (playerChoice !== "kamień" && playerChoice !== "papier" && playerChoice !== "nożyce") {
+      alert("Nieprawidłowy wybór. Spróbuj jeszcze raz.");
+      startGame(); // Ponowne wywołanie funkcji, jeśli wybór był nieprawidłowy
+      return;
+    }
+    
+    const computer = computerChoice();
+    const result = playRound(playerChoice, computer);
+    
+    alert(result);
+    
+    const playAgain = confirm("Czy chcesz zagrać jeszcze raz?");
+    if (playAgain) {
+      startGame(); // Ponowne wywołanie funkcji, jeśli gracz chce zagrać jeszcze raz
+    } else {
+      alert("Dziękujemy za grę!");
+    }
+  }
+  
+  // Rozpoczęcie gry po załadowaniu strony
+  startGame();
