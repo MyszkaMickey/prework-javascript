158c881dde44eda9a12630b8f7c0db827c05632a
Mateusz Barej <mateusz.barej89@gmail.com>
2023-06-22 13:03:19 +0200

    add wrong choice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var playerMove;
-playerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -15,3 +11,18 @@ if (randomNumber === 1) {
   computerMove = 'nieznany ruch';
 }
 console.log('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput === '1') {
+  playerMove = 'kamień';
+} else if (playerInput === '2') {
+  playerMove = 'papier';
+} else if (playerInput === '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+  printMessage('Błędny wybór! Twój ruch został ustawiony na kamień.');
+}
+console.log('Twój ruch: ' + playerMove);
